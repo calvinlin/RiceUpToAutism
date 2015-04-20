3f5242fd532ecf22f7992db52629522802166cea
kanirolf <wdomingo@uci.edu>
2015-04-19 22:15:11 -0700

    Added more stuff to egg level.

--- egg.game.js
+++ egg.game.js
@@ -16,35 +16,31 @@ function ConveyorBelt( sequencer, speed, updateInterval, element, eggElement ){
 
 	this.sequencer = sequencer;
 	
-	this.speed = speed === undefined ? 8000 : speed;
 	this.updateInterval = updateInterval === undefined ? 500 : updateInterval;
 	this.selector = element === undefined ? "#conveyor-belt" : element;
 	this.eggSelector = eggElement === undefined ? ".egg" : eggElement;
 	
 	this.element = $(this.selector);
 	this.eggElement = $(this.eggSelector); 
+	
+	this.setSpeed(speed === undefined ? 8000 : speed);
+	
+	this.dropped = 0;
 
 	this.fireOnEmpty = function(){};
-	
-	this.element.css({ 
-		"animation-duration": (this.speed / 7).toString() + "ms",
-		"-webkit-animation-duration": (this.speed / 7).toString() + "ms",
-		"-moz-animation-duration": (this.speed / 7).toString() + "ms",
-		"-o-animation-duration": (this.speed / 7).toString() + "ms"
-	});
 	
 	interact(this.selector).dropzone({
 		accept: this.eggSelector,
 		overlap: 0.2,
 		ondrop: function (event){
 			var yDisp = parseFloat($(event.relatedTarget).css("transform").split(",")[5]);
-			if (yDisp <= 0 && yDisp >= -90){
+			if (yDisp <= 50 && yDisp >= -90){
 				$(event.relatedTarget).addClass("on-conveyor");
 			}
 		}
 	});
 	
-	interact(this.eggSelector)
+	interact(this.selector + " > " + this.eggSelector)
 	.on("mousedown", function(event){
 		$(event.target).removeClass("on-conveyor");
 	})
@@ -61,7 +57,7 @@ function ConveyorBelt( sequencer, speed, updateInterval, element, eggElement ){
 			}, 0, "linear");
 		},
 		onend: function(event){
-			$(event.target).not(".on-conveyor")
+			$(event.target).not(".on-conveyor, .on-nest")
 				.transition({
 					y: "-=100"
 				}, 100, "ease-in-out", function (){
@@ -73,7 +69,8 @@ function ConveyorBelt( sequencer, speed, updateInterval, element, eggElement ){
 							$(this).remove();
 						});
 				});
-		}
+			++this.dropped;
+		}.bind(this)
 	});
 	
 	
@@ -86,11 +83,13 @@ function ConveyorBelt( sequencer, speed, updateInterval, element, eggElement ){
 		var eggs = $(this.selector + " > " + this.eggSelector);
 		
 		if (eggs.length > 0){
+			var self = this;
 			eggs.filter(".on-conveyor").each(function(){	
 				var newPos = parseFloat($(this).css("transform").split(",")[4]) + dx;
 				if (newPos < 1280){
 					$(this).transition({ x: newPos }, updateInterval, "linear");
 				} else {
+					++self.dropped;
 					$(this).remove();
 				}
 			});
@@ -103,10 +102,25 @@ function ConveyorBelt( sequencer, speed, updateInterval, element, eggElement ){
 	updateEggPos();
 }
 
+ConveyorBelt.prototype.setSpeed = function (speed, blocking){
+	this.sequencer.newFunction( blocking, function(next){
+	
+	this.speed = speed;
+	this.element.css({ 
+		"animation-duration": (this.speed / 7).toString() + "ms",
+		"-webkit-animation-duration": (this.speed / 7).toString() + "ms",
+		"-moz-animation-duration": (this.speed / 7).toString() + "ms",
+		"-o-animation-duration": (this.speed / 7).toString() + "ms"
+	});
+	
+}.bind(this))};
+
 ConveyorBelt.prototype.spawnEgg = function( color, blocking ){
 	this.sequencer.newFunction ( blocking, function( next ){
 		
-	$("<div class='egg on-conveyor " + color + "'></div>").appendTo(this.element);
+	$("<div class='egg on-conveyor " + color + "'></div>")
+		.css("background", color)
+		.appendTo(this.element);
 	window.setTimeout(next, 120 / (120 + 1280) * this.speed);
 	
 }.bind(this))};
@@ -120,29 +134,73 @@ ConveyorBelt.prototype.waitOnClear = function( blocking ){
 
 function Nest ( sequencer, element, accepts, eggElement ){
 	
-	this.selector = element === undefined ? ".nest" : element; 
-	this.accepts = accepts === undefined ? false : accepts;
+	this.selector = element === undefined ? ".nest#nest-" + (++Nest.NUM_OF_INSTANCES) : element; 
+	this.accepts = accepts === undefined ? true : accepts;
 
-	interact.dropzone({
-		
-		
+	this.nDropped = 0;
+	
+	this._isAccepted = function(element){
+		return this.accepts === true || 
+			   (typeof this.accepts === "string" &&
+			    element.classList.contains(this.accepts)) ||
+			   (this.accepts instanceof Array && 
+				this.accepts.reduce(function(prev, curr){ 
+					return element.classList.contains(curr) || prev
+				}, false)
+		);
+	}.bind(this);
+	
+	interact(this.selector).dropzone({
+		overlap: 0.1,
+		ondrop: function (event){
+			if (this._isAccepted(event.relatedTarget)){
+				++this.nDropped;
+				$(event.relatedTarget)
+					.addClass("on-nest")
+					.remove()
+					.appendTo(this.selector)
+					.transition({
+						x: 0,
+						y: 0
+					}, 0, "linear");	
+			}
+		}.bind(this)
 	});
 }
 
+Nest.NUM_OF_INSTANCES = 0;
+
 var sequencer = new Sequential();
-
 var sleep = new Sleeper(sequencer);
-var conveyor = new ConveyorBelt( sequencer, 8000, 50 );
+
+var conveyor = new ConveyorBelt( sequencer, 6000, 5 );
+var dialog = new Dialog( sequencer, undefined, "#dialog-box", "#dialog-head");
+
+var nest1 = new Nest(sequencer, undefined, "green");
+var nest2 = new Nest(sequencer, undefined, false);
+var nest3 = new Nest(sequencer, undefined, false);
+
+dialog.printDialog("Put the eggs into the basket with the correct color.");
 
 conveyor.spawnEgg("green", BLOCKING);
 sleep.ms(1000, BLOCKING);
-conveyor.spawnEgg("green", BLOCKING);
+conveyor.spawnEgg("purple", BLOCKING);
+
 conveyor.waitOnClear(BLOCKING)
-conveyor.spawnEgg("green", BLOCKING);
-sleep.ms(1000, BLOCKING);
-conveyor.spawnEgg("green", BLOCKING);
-sleep.ms(1000, BLOCKING);
+conveyor.setSpeed(4000, BLOCKING);
 
 conveyor.spawnEgg("green", BLOCKING);
+sleep.ms(1000, BLOCKING);
+conveyor.spawnEgg("pink", BLOCKING);
+sleep.ms(1000, BLOCKING);
+conveyor.spawnEgg("green", BLOCKING);
 
+conveyor.waitOnClear(BLOCKING);
+sleep.ms(1000, BLOCKING);
+sequencer.newFunction(BLOCKING, function (next){
+	console.log("hello");
+	document.getElementById("greyout").style.display = "block";
+	document.getElementById("tally-box").style.display = "block";
+	next();
+});
 }
