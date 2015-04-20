42c2f595a5fac47875bda7aa75c4aa899af609e5
Calvin L <Calvin@dhcp-v103-122.mobile.uci.edu>
2015-04-19 22:48:10 -0700

    fixed some things, added config for debugging
    music plays correctly
    config.html should show localStorage data

--- MainPage.js
+++ MainPage.js
@@ -1,12 +1,50 @@
 window.onload = function (){
-	init();
+	
 	function init() {
 		//if task is not bought yet, change CSS to grey
 		//load money amount
 		//load XP amount
 		$('.layer').hide();
 		$('#_mainScreen').show();
+		startMusic();
 	}
+	
+	var mainMusic = new Audio('audio/bgm.mp3');
+	var taskMusic = new Audio('audio/task.mp3');
+	init();
+	
+//=============================================================================
+// MUSIC FUNCTIONALITY
+//-----------------------------------------------------------------------------
+// Remembers music file locations, starts/stops when necessary 
+//-----------------------------------------------------------------------------
+		$('#music').toggle(
+				function () {
+					startMusic();
+				},
+				function () {
+					stopMusic();
+				}
+		);
+		
+		function stopMusic() {
+			mainMusic.pause();
+			mainMusic.currentTime = 0;
+			
+			taskMusic.pause();
+			taskMusic.currentTime = 0;
+		}
+		
+		function startMusic() {
+			stopMusic();
+			if($('#_mainScreen').is(':visible')) {
+				mainMusic.play();
+			}
+			else {
+				taskMusic.play();
+			}
+		}
+//=============================================================================
 
 	
 //=============================================================================
@@ -21,9 +59,8 @@ window.onload = function (){
 	}
 	,function(){
 		$(this).css({ transform: 'scale(1)' });
-	});        
+	});
 
-	
 //=============================================================================
 
 //=============================================================================
@@ -52,33 +89,39 @@ window.onload = function (){
 		navigate('2');
 	});
 
-//-----------------------------------------------------------------------------
+
 	function navigate(data) {
-		alert(data);
+		stopMusic();
 		$('#_mainPage').hide();
 		if(data == '11') {
 			$('.layer').hide();
 			$('#_task1').show();
+			taskMusic.play();
 		}
 		else if(data == '12') {
 			$('.layer').hide();
 			$('#_task2').show();
+			taskMusic.play();
 		}
 		else if(data == '13') {
 			$('.layer').hide();
 			$('#_task3').show();
+			taskMusic.play();
 		}
 		else if(data == '14') {
 			$('.layer').hide();
 			$('#_task4').show();
+			taskMusic.play();
 		}
 		else if(data == '1') {
 			$('.layer').hide();
 			$('#_shop').show();
+			taskMusic.play();
 		}
 		else if(data=='2') {
 			$('.layer').hide();
 			$('#_shop').show();
+			taskMusic.play();
 		}
 	}
 //=============================================================================
