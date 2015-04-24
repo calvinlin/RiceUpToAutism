f1dc9ce751ce400f2dbf9e1f983fba8b44e80e68
kanirolf <wdomingo@uci.edu>
2015-04-24 14:54:22 -0700

    omg...

--- dialog-module.js
+++ dialog-module.js
@@ -50,7 +50,7 @@ Dialog.prototype._dialog = function(callback, dialog, image, align){
 		}	
 	}.bind(this);
 	
-	$(currentLayer).one("click", function(){
+	this.element.one("click", function(){
 		textBox.text(this.element.text() + letters.join(""));
 		window.clearTimeout(toRemove);
 		this.head.removeClass("talking");
@@ -89,6 +89,6 @@ Dialog.prototype.promptNext = function (blocking) {
 	this.sequencer.newFunction(blocking, function(next){
 		
 	this.element.append('<div class="dialog-box-arrow"></div><div class="dialog-continue">click to continue</div>');
-	$(currentLayer).one("click", next);
+	this.element.one("click", next);
 	
 }.bind(this))};
